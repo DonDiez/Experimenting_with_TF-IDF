584ce42cfb8a4e784da9def59b33a6c45503fae2
Thomas 'Tartarux' Ramirez <thomasrf96@gmail.com>
2020-08-27 15:52:04 +0200

    SearchEngine done

--- app.py
+++ app.py
@@ -3,17 +3,18 @@ import os
 from os import listdir
 from os.path import isfile, join
 from tdidf import search
+import string
+
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
-    if request.method == "POST":
-        resultList = (request.form["searchQuery"])
-        print(search(resultList))
-        return render_template("index.html", results=search(resultList))
+    if request.method == "POST" and str(request.form["searchQuery"])!= "":
+        resultList = search((request.form["searchQuery"]))
+        return render_template("index.html", results=resultList, playSound=True)
     else:
-        return render_template("index.html")
+        return render_template("index.html",playSound=False)
 
 @app.route('/files')
 def method_name():
@@ -23,6 +24,22 @@ def method_name():
     # q = input(str("\nQuery? : "))
     listen = []
     return render_template("index.html", files=onlyfiles)
+
+
+
+
+
+@app.route('/page/<pageTitle>', methods=["GET","POST"])
+def filePage(pageTitle):
+    title = (str(pageTitle)[0]).upper()+ str(pageTitle)[1:-4]
+    f = open("files/"+pageTitle, "r")
+    body=""
+    for lines in f:
+        body+= lines+"\n"
+    print(body)
+    return render_template("page.html", title=title.replace("_"," "), body=body)
+
+
 
 # Removes the cache of the CSS on the flask development server
 @app.context_processor
